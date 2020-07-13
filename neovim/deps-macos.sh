fb2550ffd22764ed8401d2e6e8ffccd95e3756e1
Adebayo Akinlaja <adebayo.tag@gmail.com>
2020-07-14 00:29:59 +0100

    (neovim) fix neovim installation and removed unused custom zsh commands

--- neovim/deps-macos.sh
+++ neovim/deps-macos.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-brew install python2
+brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/86a44a0a552c673a05f11018459c9f5faae3becc/Formula/python@2.rb
 brew install python3
 pip2 install neovim --upgrade
 pip3 install neovim --upgrade
-
+brew install neovim
