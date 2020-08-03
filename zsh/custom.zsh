f06de08171acbbd0b8fd4116f8a600d0bf3ae6cd
Adebayo Akinlaja <adebayo@mercari.com>
2020-08-03 20:23:14 +0100

    (zsh config) make changes to zsh configs

--- zsh/custom.zsh
+++ zsh/custom.zsh
@@ -12,6 +12,10 @@
 # alias kubectl_context="kubectl config current-context"
 # alias kube_wpod="watch kubectl get po"
 
+# export GOOGLE_CLOUD_SDK=$HOME/google/google-cloud-sdk/
+# source $GOOGLE_CLOUD_SDK/completion.zsh.inc
+# source $GOOGLE_CLOUD_SDK/path.zsh.inc
+
 # The next line updates PATH for the Google Cloud SDK.
 # if [ -f '/Users/adebayo/google/google-cloud-sdk/path.zsh.inc' ]; then source '/Users/adebayo/google/google-cloud-sdk/path.zsh.inc'; fi
 
@@ -21,9 +25,5 @@
 # export PATH="$PATH:$HOME/protoc-3.2/bin"
 # export PATH="$PATH:$HOME/google/cloud-sql-proxy"
 # export CLOUDSQLPROXYPATH=$HOME/scratches/certificates/microservices-9e02236eb849.json
-
-# export GOOGLE_CLOUD_SDK=$HOME/google/google-cloud-sdk/
-# source $GOOGLE_CLOUD_SDK/completion.zsh.inc
-# source $GOOGLE_CLOUD_SDK/path.zsh.inc
 
 # source <(kubectl completion zsh)
